7577d5994035394c52db469cb9e7e67ce89bcd1f
Alexander Treptau <atreptau@welfenlab.de>
2016-02-05 14:58:49 +0100

    (fix) Draw graphs with jailed works now

--- src/chart_processor.js
+++ src/chart_processor.js
@@ -6,12 +6,48 @@ d3 = require('d3')
 nvd3 = require('nvd3')
 uuid = require('node-uuid')
 //jailed = require('jailed');
+esprima = require('esprima');
 
 parseInput = function (input) {}
 
+defaultTimout = 5000;
+
+getCode = function(input) {
+  return '\
+      var __plotData__ = [];\
+      var _plot = function(input) {\
+        if (typeof(input) == "function")\
+          return input();\
+        return input;\
+      };\
+      var plot = function() {\
+        for(var _i = 0; _i < arguments.length; _i++) {\
+          __plotData__.push(_plot(arguments[_i]));\
+        }\
+      };\
+      var start = function (app) {\
+        this.application = null;\
+        __plotData__ = [];\
+        run();\
+        app.remote.__data__(__plotData__);\
+        app.remote.__finished__();\
+      };\
+      var run = function () {\
+        var start = null;' + input + '\
+      };\
+      var globalScope;\
+      if (typeof(window) === "undefined")\
+        globalScope = this;\
+      else\
+        globalScope = window;\
+      start.call(globalScope, application);\
+    '
+}
+
 graphChartProcessor = function (tokens, graph_template, error_template) {
-  parseInput = function (input) {
-    var working, customApi, disconnect;
+  parseInput = function (input, doneCB) {
+    console.time("Graph-Chart");
+    var working, customApi, disconnect, code;
     var out = [];
 
     working = true
@@ -21,57 +57,54 @@ graphChartProcessor = function (tokens, graph_template, error_template) {
     customApi.remote["__data__"] = function (remoteData) {
       out.push(remoteData)
     }
+
     customApi.remote["__finished__"] = function () {
-        working = false
-        console.log('finished calcs ', out)
-        disconnect()
-    }
-
-    code2 = '\
-      var run = function () {\
-        var start = null;' + input +
-      '}\
-      var start = function (app) {\
-        this.application = null;\
-        var _plot = function(input) {\
-          if (typeof(input) == "function")\
-            return input();\
-          return input;\
-        }\
-        var __plotData__ = []\
-        var plot = function() {\
-          for(var _i = 0; _i < arguments.length; _i++) {\
-            __plotData__.push(_plot(arguments[_i]));\
-          }\
-        }\
-        run();\
-        app.remote.__data__(__plotData__);\
-        app.remote.__finished__();\
-      }\
-      var globalScope;\
-      if (typeof(window) === "undefined")\
-        globalScope = this\
-      else\
-        globalScope = window\
-      start.call(globalScope, application);\
-      '
-
-    runner = new jailed.DynamicPlugin(code2, customApi.remote);
-    disconnect = runner.disconnect.bind(runner);
-
-    var data = []
-    var part, line, xyArray, len, len1
-    for (var j = 0, len = out.length; j < len; j++) {
-      part = out[j]
-      line = []
-      xyArray = []
-      for (var k = 0, len1 = part.length; k < len1; k++) {
-        i = part[k]
-        xyArray.push({ x: i[0], y: i[1]})
+      working = false
+      out = out[0]
+      var data = []
+      var part, line, xyArray, len, len1, obj0;
+      for (var j = 0, len = out.length; j < len; j++) {
+        part = out[j]
+        line = []
+        xyArray = []
+        for (var k = 0, len1 = part.length; k < len1; k++) {
+          obj0 = part[k]
+          xyArray.push({ x: obj0[0], y: obj0[1]})
+        }
+        data.push({ values: xyArray, seriesIndex: j, key: '' + j })
       }
-      data.push({ values: xyArray, seriesIndex: j, key: '' + j })
+      //console.log(data)
+      //console.log('finished calcs', out, data)
+      console.timeEnd("Graph-Chart");
+      console.time("Graph-Chart-Plot");
+      doneCB(data);
+      console.timeEnd("Graph-Chart-Plot");
+      disconnect()
+      return disconnect;
     }
-    return data
+
+    code = getCode(input);
+
+    try {
+      esprima.parse(code);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+    runner = new jailed.DynamicPlugin(code, customApi.remote);
+    runner.whenFailed(function () {
+      console.warn("jailed.DynamicPlugin failed while ploting/calculating");
+    });
+    disconnect = runner.disconnect.bind(runner);
+    if (defaultTimout > 0)
+      setTimeout((function() {
+        var ref1;
+        if (working) {
+          console.log("(Graph-Chart-) Sandbox timed out after " + defaultTimout + "ms!");
+          console.timeEnd("Graph-Chart");
+          return runner.disconnect();
+        }
+      }), defaultTimout);
   }
   return {
     parse: parseInput,
@@ -89,21 +122,23 @@ graphChartProcessor = function (tokens, graph_template, error_template) {
         prevCodeStr += tokens[prevJSToken].content;
 
         try {
-          var data = parseInput(prevCodeStr + inputStr)
           var id = 'charts-dg-' + uuid.v4()
           postProcessors.registerElemenbById(id, function (elem, done) {
-            var fnc, svgElem, svgHeight
-            fnc = function () {
-              var chart
-              chart = nvd3.models.lineChart()
-              chart.useInteractiveGuideline(false) // buggy
-              d3.select(elem.childNodes[0]).datum(data).call(chart)
-              return chart
+            createGraph = function (data) {
+              fnc = function () {
+                var chart
+                chart = nvd3.models.lineChart()
+                chart.useInteractiveGuideline(false) // buggy
+                d3.select(elem.childNodes[0]).datum(data).call(chart)
+                return chart
+              }
+              nvd3.addGraph(fnc)
+              svgElem = elem.getElementsByClassName('output')[0]
+              svgHeight = (svgElem != null ? svgElem.getBoundingClientRect().height : void 0) || 0
+              elem.style.height = 350
             }
-            nvd3.addGraph(fnc)
-            svgElem = elem.getElementsByClassName('output')[0]
-            svgHeight = (svgElem != null ? svgElem.getBoundingClientRect().height : void 0) || 0
-            elem.style.height = 350
+
+            parseInput(prevCodeStr + inputStr, createGraph)
             return done()
           })
           return graph_template({
